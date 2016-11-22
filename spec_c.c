2a6ebd0eaff1f3639d69978c0dcd3c9ff4ca6364
Richard Sim <rdsim8589@gmail.com>
2016-11-22 00:01:49 +0000

    complete spec_c.c according for all tags (not tested)

--- spec_c.c
+++ spec_c.c
@@ -1,19 +1,59 @@
 #include "holberton.h"
-#include <stdio.h>
 /**
- *
+ * spec_c - sends va_arg with appropriate tags into buffer
+ * @b_r: a pointer to the struct buffer
+ * @t: a pointer to the struct tags
  */
 void _spec_c(buffer *b_r, tags *t)
 {
 	char hold;
+	char *b_str;
+	int i ,j ,k , minus, b_str_size;
 
 	if (b_r != NULL)
 	{
 		/* get arg from va_arg and store */
 		hold = va_arg(b_r->ap, int);
-		/* determine the len of arg*/
-		/* do the tag stuff*/
-		/* add into buffer and advance buffer*/
-		b_r->buf[b_r->bp++] = hold;
+		minus = 0;
+		/* if width if found */
+		if (t->width > 1)
+		{
+			b_str_size = t->width;
+			b_str = malloc(b_str_size * sizeof(char));
+			/*if - flag is found */
+			k = 0;
+			while (t->flags[k] != '\0')
+			{
+				if (t->flags[k] == '-')
+					minus = 1;
+				k++;
+			}
+			i = 0;
+			/*- flag found, hold left most */
+			if (minus == 1)
+			{
+				b_str[i++] = hold;
+				while (i < b_str_size)
+					b_str[i++] = ' ';
+			}
+			/* - flag not found, hold right most*/
+			else
+			{
+				while (i < b_str_size - 1)
+					b_str[i++] = ' ';
+				b_str[i] = hold;
+			}
+		}
+		/* no width given */
+		else
+		{
+			b_str_size = 1;
+			b_str = malloc(b_str_size * sizeof(char));
+			b_str[0] = hold;
+		}
+		j = 0;
+		while (j < b_str_size)
+			b_r->buf[b_r->bp++] = b_str[j++];
 	}
+	free(b_str);
 }
